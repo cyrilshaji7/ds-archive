f77b20a6dd324fb257f4d94a7002e342a63e4177
Cyril Shaji <cyrilshaji7@gmail.com>
2024-07-26 21:06:47 -0400

    idk

--- run.py
+++ run.py
@@ -1,8 +1,12 @@
 # run.py
 from app import create_app
+from flask import Flask, redirect
 from app.routes import api
 
 if __name__ == "__main__":
     app = create_app()
     app.register_blueprint(api, url_prefix='/api')
+    @app.route('/')
+    def home():
+        return redirect('/api/')
     app.run(debug=True)
